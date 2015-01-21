2eabe80e2181b5fb38e0ecea06990ec2398736d3
Justin Dray <justin@dray.be>
2015-01-21 15:42:20 +1000

    recommented package cache as it should be. .mount_packages.sh script should uncomment it in the correct circumstances

--- configs/releng/airootfs/root/.mount_packages.sh
+++ configs/releng/airootfs/root/.mount_packages.sh
@@ -5,7 +5,7 @@ while [[ $count != 10 ]]
 do
 	if nslookup abachi.dray.be | grep -q 192.168.1.15
 	then
-		if ip route | grep default | grep 192.168.1.1
+		if ip route | grep default | grep -q 192.168.1.1
 		then
 			sed -i 's/^#//g' /etc/fstab
 			mount /var/cache/pacman/pkg
