aab4fa9350d25380b3555ca48fd34d075ef0fb50
Justin Dray <justin@dray.be>
2015-04-23 21:27:55 +1000

    Added avahi

--- configs/releng/airootfs/root/customize_airootfs.sh
+++ configs/releng/airootfs/root/customize_airootfs.sh
@@ -26,5 +26,5 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service sshd.service
+systemctl enable pacman-init.service sshd.service avahi-daemon.service
 systemctl set-default multi-user.target
