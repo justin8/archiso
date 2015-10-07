7d20cb05a43ebfd4384d9d66e61dc8407aced5a9
Justin Dray <justin@dray.be>
2015-10-07 22:57:12 +0200

    Cleaned up and made closer to upstream

--- configs/releng/airootfs/root/customize_airootfs.sh
+++ configs/releng/airootfs/root/customize_airootfs.sh
@@ -2,20 +2,19 @@
 
 set -e -u
 
-sed -i 's/#\(en_AU\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh arch
 echo "root:root" | chpasswd
 
 chmod 750 /etc/sudoers.d
-chmod 440 /etc/sudoers.d/g_wheel
+chmod 440 /etc/sudoers.d/wheel
 chmod 700 /root/.ssh
 chmod 600 /root/.ssh/authorized_keys
 
